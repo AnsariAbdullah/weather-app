9465e4e7ac4b99b758e9ce40b342675a5ca26c3f
AnsariAbdullah <ansariabdullahar@gmail.com>
2020-01-06 08:47:23 +0530

    add proxy

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ window.addEventListener('load', () => {
 			long = position.coords.longitude;
 			lat = position.coords.latitude;
 			console.log(long, lat);
-			const api = `https://api.darksky.net/forecast/1375af5e029e8ae2e530d615f99da203/${lat},${long}`
+			const proxy = 'https://cors-anywhere.herokuapp.com/'
+			const api = `${proxy}https://api.darksky.net/forecast/1375af5e029e8ae2e530d615f99da203/${lat},${long}`
 			fetch(api)
 				.then(response => {
 					return response.json();
