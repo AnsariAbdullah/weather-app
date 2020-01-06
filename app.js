8531aa5af9f4701ba67c1d635e299836bc39d175
AnsariAbdullah <ansariabdullahar@gmail.com>
2020-01-06 07:54:09 +0530

    basic styling and js

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-window.addEventListener('load', ()=>{
+window.addEventListener('load', () => {
 	let long;
 	let lat;
+
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(position=>{
+			console.log(position);
+			long = position.coords.longitude;
+			lat = position.coords.latitude;
+			console.log(long, lat);
+		})
+	}
 });
