a02a5d19acf5bfbd6e6738c7d335ed4478e2f78d
AnsariAbdullah <ansariabdullahar@gmail.com>
2020-01-06 08:32:01 +0530

    fetch api data

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ window.addEventListener('load', () => {
 			long = position.coords.longitude;
 			lat = position.coords.latitude;
 			console.log(long, lat);
+			const api = `https://api.darksky.net/forecast/1375af5e029e8ae2e530d615f99da203/${lat},${long}`
+			fetch(api)
+				.then(response => {
+					return response.json();
+				})
+				.then(data => {
+					console.log(data);
+				})
 		})
 	}
 });
